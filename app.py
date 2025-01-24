81d4bb190161136fd729e75859d5f53d431bcebe
MuiOP <natthakit.ka.67@ubu.ac.th>
2025-01-24 14:03:39 +0700

    สร้างคลาสแรกบน Github repo

---
+++ app.py
@@ -0,0 +1,11 @@
+ class Student(object)
+   Student_count = 0
+   def _new_(self):
+     print("Student._new_")
+
+   def _init_(self):
+     print("Student._init_")
+     self.gpa = 4.00
+
+   def study_hard(self):
+     print("sir, yes sir.")
